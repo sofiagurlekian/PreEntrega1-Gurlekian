d8a5b0de6f3771c1578e21fd474803bda0848207
Sofia Gurlekian <sofiagurlekian@gmail.com>
2023-07-25 14:45:35 -0300

    suxth commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let nombre = prompt("Ingrese su nombre");
 let apellido = prompt ("Ingrese su apellido");
 function saludar(){
     let saludo = "Bienvenido/a a TRAM: su casa de cambio de confianza."
-    alert(saludo);
+    alert (saludo);
 }
 
 while (nombre === "" || apellido === "") {
@@ -16,36 +16,65 @@ while (nombre === "" || apellido === "") {
 saludar();
 
 let edad = Number(prompt("Ingrese su edad"));
+let valorArs = 1;
+let valorUsd = 550;
+let valorBrl = 100;
+let valorUyu = 50;
+
 function tipoDeCambio() {
     let moneda = prompt("Ingrese el código de la moneda con la que desea operar").toLowerCase();
 
-    let valorArs = 1;
-    let valorUsd = 550;
-    let valorBrl = 100;
-    let valorUyu = 50;
-
     switch (moneda) {
         case "ars":
-            alert("Moneda de Argentina\n1 ARS = " + valorArs + " ARS");
+            alert ("Moneda de Argentina\n1 ARS = " + valorArs + " ARS");
             break;
 
         case "usd":
-            alert("Moneda de Estados Unidos\n1 USD = " + valorUsd + " ARS");
+            alert ("Moneda de Estados Unidos\n1 USD = " + valorUsd + " ARS");
             break;
 
         case "brl":
-            alert("Moneda de Brasil\n1 BRL = " + valorBrl + " ARS");
+            alert ("Moneda de Brasil\n1 BRL = " + valorBrl + " ARS");
             break;
 
         case "uyu":
-            alert("Moneda de Uruguay\n1 UYU = " + valorUyu + " ARS");
+            alert ("Moneda de Uruguay\n1 UYU = " + valorUyu + " ARS");
             break;
 
         default:
-            alert("Moneda no reconocida");
+            alert ("Moneda no reconocida");
             break;
     }
 }
+
+function convertirDivisas(cantidad, tasaDeCambio) {
+  // Utilizamos un switch para determinar la tasa de cambio según la moneda de origen
+  switch (monedaOrigen) {
+      case "ars":
+          cantidadConvertida = convertirDivisas(cantidad, valorArs);
+      break;
+
+      case "usd":
+          cantidadConvertida = convertirDivisas(cantidad, valorUsd);
+      break;
+
+      case "brl":
+          cantidadConvertida = convertirDivisas(cantidad, valorBrl);
+      break;
+
+      case "uyu":
+          cantidadConvertida = convertirDivisas(cantidad, valorUyu);
+      break;
+
+      default:
+          cantidadConvertida = "Moneda no reconocida";
+      break;
+      }
+
+      alert(`Cantidad convertida: ${cantidadConvertida}`);
+
+      return convertirDivisas;
+}    
 
 
 
@@ -57,23 +86,26 @@ if (edad >= 18){
         } else{
             alert ("Puede operar en el mercado cambiario.");
 
-           //Saque un turno para realizar el cambio de divisas
+           //Para realizar el cambio de divisas debe ingresar su nro de DNI, el de su madre y el de su padre
 
-            for (let turno = 1; turno <= 10; turno++) {
-                let dni = Number(prompt("Ingrese su DNI con el que efectuará la transacción"));
-                   alert(`Turno #${turno} DNI ${dni}`);
+            for (let i = 1; i <= 3; i++) {
+                let dni = Number(prompt("Ingrese primero su DNI, luego el DNI de su madre, y por último, el DNI de su padre"));
+                   alert(`Número #${i} DNI ${dni}`);
            }
-           alert ("Se acabaron los turnos")
+           alert ("Se registraron los 3 DNIs")
 
            //Seleccione la moneda
            tipoDeCambio();
-           
-        }    
-} else{
+
+           // Pedimos al usuario que ingrese la cantidad a convertir y la moneda de origen
+        let cantidad = parseFloat(prompt("Ingrese la cantidad de dinero que desea cambiar"));
+        let monedaOrigen = prompt("Ingrese el código de la moneda de origen (ARS, USD, BRL o UYU)").toLowerCase();
+        let cantidadConvertida;
+           convertirDivisas (cantidad, tasaDeCambio);
+        }
+}else {
     alert ("No puede operar en el mercado cambiario.");
 }
-
-
     
 
 
