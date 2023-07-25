c46b6f3316c0f0f55d7eb53b2cdf1ca10189d841
Sofia Gurlekian <sofiagurlekian@gmail.com>
2023-07-25 01:20:01 -0300

    fifth commit

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt ("Ingrese su apellido");
+function saludar(){
+    let saludo = "Bienvenido/a a TRAM: su casa de cambio de confianza."
+    alert(saludo);
+}
 
 while (nombre === "" || apellido === "") {
         alert("Debe ingresar nombre y apellido.");
@@ -9,18 +13,49 @@ while (nombre === "" || apellido === "") {
         apellido = prompt("Ingrese su apellido");
 } 
 
-// let saludo = "Bienvenido/a"
-// let mensajeCompleto = saludo + " " + nombre + " " + apellido;
-//     alert(mensajeCompleto);
+saludar();
 
 let edad = Number(prompt("Ingrese su edad"));
+function tipoDeCambio() {
+    let moneda = prompt("Ingrese el código de la moneda con la que desea operar").toLowerCase();
+
+    let valorArs = 1;
+    let valorUsd = 550;
+    let valorBrl = 100;
+    let valorUyu = 50;
+
+    switch (moneda) {
+        case "ars":
+            alert("Moneda de Argentina\n1 ARS = " + valorArs + " ARS");
+            break;
+
+        case "usd":
+            alert("Moneda de Estados Unidos\n1 USD = " + valorUsd + " ARS");
+            break;
+
+        case "brl":
+            alert("Moneda de Brasil\n1 BRL = " + valorBrl + " ARS");
+            break;
+
+        case "uyu":
+            alert("Moneda de Uruguay\n1 UYU = " + valorUyu + " ARS");
+            break;
+
+        default:
+            alert("Moneda no reconocida");
+            break;
+    }
+}
+
+
+
 
 if (edad >= 18){
     let subsidio = prompt ("Es usted beneficiario del subsidio a las tarifas de electricidad, gas y agua?").toLowerCase();
         if(subsidio === "si"){
             alert ("No puede operar en el mercado cambiario");
         } else{
-            alert ("Puede operar en el mercado cambiario.")
+            alert ("Puede operar en el mercado cambiario.");
 
            //Saque un turno para realizar el cambio de divisas
 
@@ -31,30 +66,8 @@ if (edad >= 18){
            alert ("Se acabaron los turnos")
 
            //Seleccione la moneda
-
-            let moneda = prompt("Ingrese el código de la moneda con la que desea operar").toLowerCase();
-
-            switch (moneda) {
-                case "ars":
-                alert("Moneda de Argentina");
-                break;
-
-                case "usd":
-                alert("Moneda de Estados Unidos");
-                break;
-
-                case "brl":
-                alert("Moneda de Brasil");
-                break;
-
-                case "uyu":
-                alert("Moneda de Uruguay");
-                break;
-
-                default:
-                alert("Moneda no reconocida");
-                break;
-            }
+           tipoDeCambio();
+           
         }    
 } else{
     alert ("No puede operar en el mercado cambiario.");
